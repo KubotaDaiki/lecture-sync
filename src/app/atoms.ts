b0442b68c00c63519a26c32c846bc3d431384def
daiki_win <kubotadaiki0654@gmail.com>
2024-02-14 23:16:40 +0900

    feat: 予定開始日を月曜日以外選択できないよう変更

--- src/app/atoms.ts
+++ src/app/atoms.ts
@@ -3,4 +3,4 @@ import dayjs from 'dayjs'
 
 export const scheduleAtom = atom({})
 
-export const startDateAtom = atom(dayjs())
+export const startDateAtom = atom(dayjs().subtract(dayjs().day() - 1, "day"))
--- src/app/components/DateRangeButton.tsx
+++ src/app/components/DateRangeButton.tsx
@@ -7,9 +7,22 @@ import 'dayjs/locale/ja';
 
 export default function BasicDatePicker() {
     const [startDate, setStartDate] = useAtom(startDateAtom)
+
+    const isMonday = (date) => {
+        const day = date.day();
+
+        return day !== 1;
+    };
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
-            <DatePicker label="予定開始日" sx={{ maxWidth: "200px" }} onChange={(value) => { setStartDate(value!) }} value={startDate} />
+            <DatePicker
+                label="予定開始日"
+                sx={{ maxWidth: "200px" }}
+                onChange={(value) => { setStartDate(value!) }}
+                value={startDate}
+                shouldDisableDate={isMonday}
+            />
         </LocalizationProvider>
     );
 }
